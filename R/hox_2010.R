e5f7c030a0456957af49b26abb0d78e13db5b4ab
cjvanlissa <c.j.vanlissa@uu.nl>
2020-07-16 21:16:15 +0200

    Add datasets

---
+++ R/hox_2010.R
@@ -0,0 +1,21 @@
+#' Sesame Street data based on Hox (2010)
+#'
+#' Synthetic data based on Hox (2010, p. 16).
+#'
+#' \tabular{lll}{
+#'   \strong{pupil} \tab \code{integer} \tab Pupil ID\cr
+#'   \strong{school} \tab \code{integer} \tab School ID\cr
+#'   \strong{popular} \tab \code{numeric} \tab Popularity scores,
+#'   standardized\cr
+#'   \strong{sex} \tab \code{factor} \tab Pupil sex\cr
+#'   \strong{text} \tab \code{numeric} \tab Teacher experience, standardized\cr
+#'   \strong{teachpop} \tab \code{integer} \tab ??
+#' }
+#' @docType data
+#' @keywords datasets
+#' @name hox_2010
+#' @usage data(hox_2010)
+#' @references Hox, J. J. (2010). Multilevel analysis: Techniques and
+#' applications (2nd ed.). New York, NY: Routledge.
+#' @format A data frame with 2000 rows and 6 variables.
+NULL
